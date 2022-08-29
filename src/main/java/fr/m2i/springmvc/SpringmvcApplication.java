948b18fc65bf5042edae4baf745d25f2711f1068
Slainez <sullyvan360@gmail.com>
2022-08-29 16:44:11 +0200

    Squelette et hello world en MVC Spring

---
+++ src/main/java/fr/m2i/springmvc/SpringmvcApplication.java
@@ -0,0 +1,13 @@
+package fr.m2i.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/fr/m2i/springmvc/controller/HelloController.java
@@ -0,0 +1,23 @@
+
+package fr.m2i.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    
+    @GetMapping({"/","/hello"})
+    public String showWelcomePage(ModelMap model ){
+        model.addAttribute("title", "Mon app MVC");
+        model.addAttribute("message", "Hello depuis Spring MVC");
+        
+        return "hello";
+    }
+        
+    }
+    
+ 
+    
+
